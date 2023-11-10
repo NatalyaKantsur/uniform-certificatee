6b0d46b6e4d8a2ce302830838b72346cc0ea7db9
Nataliia Kantsur <nataliia.kantsur@dbbsoftware.com>
2023-11-10 08:07:01 +0200

    cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { UniformAppProps } from '@uniformdev/context-next';
 import { UniformContext } from '@uniformdev/context-react';
 import type { RootComponentInstance } from '@uniformdev/canvas';
@@ -12,71 +11,14 @@ import '@/styles/globals.scss';
 
 const clientContext = createUniformContext();
 
-const VERCEL_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : '';
-
 const App = ({
   Component,
   pageProps,
   serverUniformContext,
-}: UniformAppProps<{ data: RootComponentInstance; context: unknown }>) => {
-  const { data: composition } = pageProps || {};
-  const {
-    pageTitle,
-    pageMetaDescription,
-    pageKeywords,
-    openGraphTitle,
-    openGraphDescription,
-    openGraphImage,
-    overlayTitleToOgImage,
-    twitterTitle,
-    twitterDescription,
-    twitterImage,
-    overlayTitleToTwitterImage,
-    twitterCard,
-  } = composition?.parameters || {};
-  //This is workaround because spaces removes from query params and not parsing automatically.
-  //Space should be encoded as %20 http://www.faqs.org/rfcs/rfc1738.html
-  const ogTitle = (openGraphTitle?.value as string)?.replaceAll?.(' ', '%20');
-  const twTitle = (twitterTitle?.value as string)?.replaceAll?.(' ', '%20');
-  const title: string = pageTitle?.value as string;
-
-  const compositionHeader = composition?.slots?.pageHeader?.[0];
-
-  const favicon = compositionHeader?.parameters?.favicon;
-
-  const renderOgImageElement = () => {
-    if (overlayTitleToOgImage?.value && openGraphImage?.value) {
-      return (
-        <meta
-          property="og:image"
-          content={`${VERCEL_URL}/api/og?title=${ogTitle ?? title?.replaceAll?.(' ', '%20')}&image=${
-            openGraphImage.value
-          }`}
-        />
-      );
-    }
-    if (openGraphImage?.value) return <meta property="og:image" content={openGraphImage?.value as string} />;
-  };
-
-  const renderTwitterImageElement = () => {
-    if (overlayTitleToTwitterImage?.value && twitterImage?.value) {
-      return (
-        <meta
-          property="twitter:image"
-          content={`${VERCEL_URL}/api/og?title=${twTitle ?? title?.replaceAll?.(' ', '%20')}&image=${
-            twitterImage.value
-          }`}
-        />
-      );
-    }
-    if (twitterImage?.value) return <meta property="twitter:image" content={twitterImage?.value as string} />;
-  };
-
-  return (
-      <UniformContext context={serverUniformContext ?? clientContext}>
-        <Component {...pageProps} providers={FakeCartContextProvider} />
-      </UniformContext>
-  );
-};
+}: UniformAppProps<{ data: RootComponentInstance; context: unknown }>) => (
+    <UniformContext context={serverUniformContext ?? clientContext}>
+      <Component {...pageProps} providers={FakeCartContextProvider} />
+    </UniformContext>
+)
 
 export default App;
